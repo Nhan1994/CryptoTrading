6b184e1748a5efecd3f37cf370f1d730c37fa65c
Nhan Nguyen <>
2026-03-04 14:59:46 +0700

    initial commit

---
+++ src/main/java/com/crypto/trading/CryptotradingApplication.java
@@ -0,0 +1,13 @@
+package com.crypto.trading;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptotradingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptotradingApplication.class, args);
+	}
+
+}
